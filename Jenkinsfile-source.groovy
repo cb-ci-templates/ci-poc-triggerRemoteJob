82da4e5c1758a66cbb98880ee421f3a9b574f902
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2026-01-09 12:09:03 +0100

    Add branch name retrieval to Jenkinsfile

--- Jenkinsfile-source.groovy
+++ Jenkinsfile-source.groovy
@@ -17,6 +17,19 @@ pipeline {
                 
                 // If you need the commit hash
                 echo "Git Commit: ${env.GIT_COMMIT}"
+                script {
+                    // Get branch name, stripping 'origin/' if present
+                    def branch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
+                    
+                    // Sometimes Jenkins checks out a specific commit (detached HEAD)
+                    // If so, you might want the commit hash instead
+                    if (branch == "HEAD") {
+                        branch = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+                    }
+                    
+                    echo "Actual Checked out ref: ${branch}"
+                }
+                    
                 // Trigger parameterized job
                 triggerRemoteJob parameterFactories: [[$class: 'SimpleString', name: 'paramKey1', value: 'paramtValueFromparent']], remotePathMissing: stopAsFailure(), remotePathUrl: getTargetInstanceID("controllers/my-target-controller","folder/my-target-job")
 
