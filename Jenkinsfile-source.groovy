5302775323f07d1fe126ea5815f57b0c749332a2
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2026-01-09 12:55:50 +0100

    Change shared library tag to 'branch1'

--- Jenkinsfile-source.groovy
+++ Jenkinsfile-source.groovy
@@ -1,4 +1,4 @@
-env.SHARED_LIB_TAG="main"
+env.SHARED_LIB_TAG="branch1"
 //library identifier: "ci-shared-library@${env.SHARED_LIB_TAG}", retriever: modernSCM(
 library identifier: "ci-shared-library", retriever: modernSCM(
         [$class: 'GitSCMSource',
