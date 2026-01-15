bc969ea003519a5cb721728cddce028a6c27cc79
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2026-01-15 10:34:18 +0100

    Remove unused parameter from Jenkinsfile
    Removed parameter 'paramKey1' from Jenkins pipeline.

--- Jenkinsfile-target.groovy
+++ Jenkinsfile-target.groovy
@@ -2,9 +2,6 @@ pipeline {
     agent {
         label 'built-in'
     }
-    parameters {
-        string defaultValue: 'paramValue1', description: 'paramKey1=paramValue1', name: 'paramKey1', trim: true
-    }
     stages {
         stage('Hello Branch1') {
             steps {
